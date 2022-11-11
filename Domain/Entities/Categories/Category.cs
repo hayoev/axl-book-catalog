e876ea8b75621ca631a78a47063ee06382e26913
Karomatullo <24tech.tj@gmail.com>
2022-11-11 16:54:01 +0500

    create configurations

--- Domain/Entities/Categories/Category.cs
+++ Domain/Entities/Categories/Category.cs
@@ -7,6 +7,7 @@ namespace Domain.Entities.Categories
 {
     public class Category : AdminAuditableBaseEntity
     {
+        public string Code { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public ICollection<Book> Books { get; set; } = new Collection<Book>();
---
+++ Infrastructure.Persistence/Configurations/Categories/CategoryConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Domain.Entities.Categories;
+
+namespace Infrastructure.Persistence.Configurations.Categories
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(b => b.Code)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(b => b.Name)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.HasOne(x => x.CreatedByAdminUser)
+                .WithMany()
+                .HasForeignKey(x => x.CreatedByAdminUserId)
+                .OnDelete(DeleteBehavior.Restrict)
+                .IsRequired();
+
+            builder.HasOne(x => x.UpdatedByAdminUser)
+                .WithMany()
+                .HasForeignKey(x => x.UpdatedByAdminUserId)
+                .OnDelete(DeleteBehavior.Restrict)
+                .IsRequired(false);
+        }
+    }
+}
